c2bed7502cb095a8eb90e0756a931b91a852d6d1
EZvirgzdsDP1-2 <ed.zvir04@inbox.lv>
2023-01-23 01:17:40 +0200

    Added template files.

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <router-view /> -->
+  <IndexPage></IndexPage>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import IndexPage from './pages/IndexPage.vue';
+
+export default defineComponent({
+  name: 'App',
+  components:{
+    IndexPage,
+  }
+})
+</script>
